bd71b91d03cc5e756fdac0050fc1fbe4fe4972fc
Jonathan Guzman <jogule@gmail.com>
2017-02-28 11:12:17 -0500

    chef-fw-rules.ps1 debug

--- chef-fw-rules.ps1
+++ chef-fw-rules.ps1
@@ -1,1 +1,2 @@
 Get-NetFirewallPortFilter | ?{$_.LocalPort -eq 5985 } | Get-NetFirewallRule | ?{ $_.Direction –eq "Inbound" -and $_.Profile -eq "Public" -and $_.Action –eq "Allow"} | Set-NetFirewallRule -RemoteAddress "Any"
+echo "FW rule executed" > c:\psdiag.log
